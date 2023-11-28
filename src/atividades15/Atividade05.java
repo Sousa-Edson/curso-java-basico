93089f8c774e97b5ac3b2316839dc9884919c1cf
Edson Sousa <edson3711.es@gmail.com>
2023-11-28 08:01:03 -0300

    adicionando mais 5 atividades

---
+++ src/atividades15/Atividade05.java
@@ -0,0 +1,34 @@
+package atividades15;
+
+import java.util.Scanner;
+
+/*Faça um programa para a leitura de duas notas parciais de um aluno.
+O programa deve calcular a média alcançada por aluno e apresentar:
+o A mensagem "Aprovado", se a média alcançada for maior ou
+igual a sete;
+o A mensagem "Reprovado", se a média for menor do que sete;
+o A mensagem "Aprovado com Distinção", se a média for igual a
+dez.*/
+public class Atividade05 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Entre com a nota 1:");
+		double nota1 = scanner.nextDouble();
+		System.out.println("Entre com a nota 2: ");
+		double nota2 = scanner.nextDouble();
+
+		double media = (nota1 + nota2) / 2.0;
+
+		if (media == 10) {
+			System.out.println("Aprovado com Distinção \nSua média é:"+ media);
+		} else if (media >= 7) {
+			System.out.println("Aprovado \nSua média é:"+ media);
+		} else {
+			System.out.println("Reprovado \nSua média é:"+ media);
+		}
+
+		scanner.close();
+	}
+
+}
