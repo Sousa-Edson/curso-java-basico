5e70bb9bbe390a6d911624cf3663cb7667fec5a4
Edson Sousa <edson3711.es@gmail.com>
2023-11-30 21:35:10 -0300

    fazendo até atividade 14

--- src/atividades15/Atividade11.java
+++ src/atividades15/Atividade11.java
@@ -23,10 +23,26 @@ public class Atividade11 {
 		System.out.println("Digite o salário: ");
 		double salario = scanner.nextDouble();
 
-		int percentual;
+		int percentual = 0;
 		if (salario <= 280) {
 			percentual = 20;
-		}else {}
+		} else if (salario >= 280 && salario <= 700) {
+			percentual = 15;
+		} else if (salario >= 700 && salario <= 1500) {
+			percentual = 10;
+		} else if (salario >= 1500) {
+			percentual = 5;
+		} else {
+			System.out.println("Erro! ");
+		}
+
+		double valorAumento = salario * percentual / 100;
+
+		System.out.println("Salário antes do reajuste "+salario);
+		System.out.println("Percentual de aumento aplicado "+percentual+"%");
+		System.out.println("Valor do aumento "+valorAumento);
+		System.out.println("Novo salário "+(salario+valorAumento));
+		
 
 		scanner.close();
 	}
---
+++ src/atividades15/Atividade13.java
@@ -0,0 +1,48 @@
+package atividades15;
+
+import java.util.Scanner;
+
+/*Faça um Programa que leia um número e exiba o dia correspondente
+da semana. (1-Domingo, 2- Segunda, etc.), se digitar outro valor deve
+aparecer valor inválido.*/
+public class Atividade13 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("Digite um número de 1 a 7: ");
+		int numero = scanner.nextInt();
+
+		String diaSemana;
+
+		switch (numero) {
+		case 1:
+			diaSemana = "Domingo";
+			break;
+		case 2:
+			diaSemana = "Segunda-feira";
+			break;
+		case 3:
+			diaSemana = "Terça-feira";
+			break;
+		case 4:
+			diaSemana = "Quarta-feira";
+			break;
+		case 5:
+			diaSemana = "Quinta-feira";
+			break;
+		case 6:
+			diaSemana = "Sexta-feira";
+			break;
+		case 7:
+			diaSemana = "Sábado";
+			break;
+		default:
+			diaSemana = "Número inválido. Tente novamente.";
+		}
+
+		System.out.println("Você digitou o número " + numero + ", que corresponde a " + diaSemana + ".");
+
+		scanner.close();
+	}
+
+}
