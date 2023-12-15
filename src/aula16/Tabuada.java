41fed2c626da13722a2d82aad8a16c6a54283eba
Edson Sousa <edson3711.es@gmail.com>
2023-12-15 08:00:40 -0300

    inserindo 2 exemplos

---
+++ src/aula16/Tabuada.java
@@ -0,0 +1,15 @@
+package aula16;
+
+ 
+public class Tabuada {
+    public static void main(String[] args) {
+        int numero = 7;
+
+        System.out.println("Tabuada do " + numero + ":");
+
+        for (int i = 1; i <= 10; i++) {
+            int resultado = numero * i;
+            System.out.println(numero + " x " + i + " = " + resultado);
+        }
+    }
+}
