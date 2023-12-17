113849d6f9be6965639813e5bc044d4d5cce4897
Edson Sousa <edson3711.es@gmail.com>
2023-12-16 22:21:53 -0300

    finalizando polimorfismo

---
+++ src/_05OrientacaoObjetosJava/herancaProtected/pacote1/Animal.java
@@ -0,0 +1,9 @@
+package _05OrientacaoObjetosJava.herancaProtected.pacote1;
+
+public class Animal {
+	protected String tipo;
+
+	protected void fazerBarulho() {
+		System.out.println("Barulho genérico");
+	}
+}
---
+++ src/_05OrientacaoObjetosJava/palavraChaveFinal/Animal.java
@@ -0,0 +1,8 @@
+package _05OrientacaoObjetosJava.palavraChaveFinal;
+
+public class Animal {
+
+    public final void fazerSom() {
+        System.out.println("Som genérico");
+    }
+}
